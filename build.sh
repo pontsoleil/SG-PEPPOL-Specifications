4b39e9849fce4b45e7894e60590e2e48924b80cb
Oscar Glanzmann <oscar.glanzmann@ecru.se>
2020-06-06 13:34:31 +0200

    generate zip file for schematron

--- build.sh
+++ build.sh
@@ -22,6 +22,12 @@ docker run --rm -i -v $PROJECT:/src anskaffelser/validator:2.1.0 build -x -t -n 
 # CEN-EN16931-CII
 #docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-CII.sch -q:tools/xquery/rules_asciidoc_cen.xquery -o:/target/CEN-EN16931-CII-GENERAL.sch.adoc
 #docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/CEN-EN16931-CII.sch -q:tools/xquery/rules_asciidoc_cen_syntax.xquery -o:/target/CEN-EN16931-CII-SYNTAX.sch.adoc
+for sch in $PROJECT/rules/sch/*.sch; do
+    docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/schematron:/target klakegg/schematron prepare /src/rules/sch/$(basename $sch) /target/$(basename $sch)
+done
+docker run --rm -i -v $PROJECT/target/site/files:/src alpine:3.6 rm -rf /src/SG-Peppol-BIS-Billing_3-Rules-Schematron.zip
+docker run --rm -i -v $PROJECT/target/schematron:/src -v $PROJECT/target/site/files:/target -w /src kramos/alpine-zip -r /target/SG-Peppol-BIS-Billing_3-Rules-Schematron.zip .
+
 
 # SG-EN16931-International Rules
 docker run --rm -i -v $PROJECT:/src -v $PROJECT/target/generated:/target --entrypoint java klakegg/saxon:9.8.0-7 -cp /saxon.jar net.sf.saxon.Query -s:/src/rules/sch/SG-Peppol-BIS-Billing_3-International-Rules.sch -q:tools/xquery/rules_asciidoc_cen.xquery -o:/target/SG-Peppol-BIS-Billing_3-International-Rules.sch.adoc
